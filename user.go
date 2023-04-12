dc1ee7f31f797e497026fd735c80fe8eae12066c
yuizhixiaocaiji <1390683299@qq.com>
2023-04-12 21:14:27 +0800

    将用户模块功能从服务模块中分离

--- user.go
+++ user.go
@@ -7,10 +7,12 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	server *Server
 }
 
 // NewUser 创建一个用户的Api
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
@@ -18,11 +20,40 @@ func NewUser(conn net.Conn) *User {
 		Addr: userAddr,
 		C:    make(chan string),
 		conn: conn,
+
+		server: server,
 	}
 
 	//启动监听当前user channel的goroutine
 	go user.ListenMessage()
 	return user
+}
+
+//用户的上线业务
+func (this *User) Online() {
+	//用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.server.BroadCast(this, "is online")
+}
+
+//用户的下线业务
+func (this *User) offline() {
+	//用户下线，将用户从onlineMap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	//广播当前用户下线消息
+	this.server.BroadCast(this, "is offline")
+}
+
+//用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
 }
 
 //监听当前User channel的方法,一旦有消息，就直接发送给对端客户端
