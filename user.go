841ba8e259d0de48fb684cc4cb3989255b2e0df7
yuizhixiaocaiji <1390683299@qq.com>
2023-04-13 22:19:06 +0800

    新增修改用户名称功能

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -29,7 +32,7 @@ func NewUser(conn net.Conn, server *Server) *User {
 	return user
 }
 
-//用户的上线业务
+// 用户的上线业务
 func (this *User) Online() {
 	//用户上线，将用户加入到onlineMap中
 	this.server.mapLock.Lock()
@@ -40,7 +43,7 @@ func (this *User) Online() {
 	this.server.BroadCast(this, "is online")
 }
 
-//用户的下线业务
+// 用户的下线业务
 func (this *User) offline() {
 	//用户下线，将用户从onlineMap中删除
 	this.server.mapLock.Lock()
@@ -51,12 +54,12 @@ func (this *User) offline() {
 	this.server.BroadCast(this, "is offline")
 }
 
-//对当前的客户端发送消息
+// 对当前的客户端发送消息
 func (this *User) SendMsg(msg string) {
 	this.conn.Write([]byte(msg))
 }
 
-//用户处理消息的业务
+// 用户处理消息的业务
 func (this *User) DoMessage(msg string) {
 	if msg == "who" {
 		//查询当前在线用户都有哪些
@@ -67,12 +70,29 @@ func (this *User) DoMessage(msg string) {
 			this.SendMsg(onlineMessage)
 		}
 		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//消息格式：rename|某某
+		newName := strings.Split(msg, "|")[1]
+
+		//判断name是否存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("您已经更新用户名：" + this.Name + "\n")
+		}
 	} else {
 		this.server.BroadCast(this, msg)
 	}
 }
 
-//监听当前User channel的方法,一旦有消息，就直接发送给对端客户端
+// 监听当前User channel的方法,一旦有消息，就直接发送给对端客户端
 func (this *User) ListenMessage() {
 	for {
 		msg := <-this.C
