538819cc09301b2fc7342781a38879acec7825e7
yuizhixiaocaiji <1390683299@qq.com>
2023-04-24 22:08:43 +0800

    新增聊天功能

--- client/client.go
+++ client/client.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -11,6 +14,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //当前client的模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +22,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	//连接server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -30,15 +35,160 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+// 处理server回应的消息，直接显示到输出
+func (client *Client) DealResponse() {
+	//一旦client.conn有数据，就直接拷贝到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+}
+
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Print(">>>>>请输入合法范围内的数字")
+		return false
+	}
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println(">>>>>请输入用户名")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	return true
+}
+
+// 查询在线用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+// 私聊模式
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+	fmt.Println(">>>>>请输入聊天对象的用户名，exit退出")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>>>请输入消息内容，exit退出")
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					log.Fatal(err)
+					break
+				}
+			}
+
+			chatMsg = ""
+			fmt.Println(">>>>请输入消息内容，exit退出")
+			fmt.Scanln(&chatMsg)
+		}
+
+		client.SelectUsers()
+		fmt.Println(">>>>>请输入聊天对象的用户名，exit退出")
+		fmt.Scanln(&remoteName)
+	}
+}
+
+func (client *Client) PublicChat() {
+	//提示用户输入消息
+	var chatMsg string
+	fmt.Println(">>>>>请输入聊天内容，exit表示退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		// 发给服务器
+		//消息不为空，则发送
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+		}
+
+		chatMsg = ""
+		fmt.Println(">>>>>请输入聊天内容，exit表示退出")
+		fmt.Scanln(&chatMsg)
+	}
+
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+
+		//根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			//公聊模式
+			client.PublicChat()
+			break
+		case 2:
+			//私聊模式
+			client.PrivateChat()
+			break
+		case 3:
+			//更新用户名
+			client.UpdateName()
+			break
+		}
+	}
+}
+
+var serverIp string
+var serverPort int
+
+// ./client -ip 127.0.0.1 -port 8888
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器ip地址（默认127.0.0.1）")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口号（默认8888）")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	//命令行解析
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>> 连接服务器失败...")
 		return
 	}
 
+	//单独开启一个goroutine处理server的回执的消息
+	go client.DealResponse()
+
 	fmt.Println(">>>>>> 连接服务器成功")
 
 	//启动客户端的业务
-	select {}
+	client.Run()
 }
